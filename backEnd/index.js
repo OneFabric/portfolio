a3e43cc0c84ebbc5b33ff17aad3c70929ed60bbb
OneFabric <1neFabric@gmail.com>
2020-06-09 10:38:20 -0700

    Restructured and modified index.js to host the right files.

--- backEnd/index.js
+++ backEnd/index.js
@@ -2,10 +2,12 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-app.use(express.static(path.join(__dirname, '../frontEnd/startbootstrap-freelancer/dist/')));
+app.use(express.static(path.join(__dirname, '../frontEnd/public')))
+app.use(express.static(path.join(__dirname, '../frontEnd/public/dist')))
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/../frontEnd/startbootstrap-freelancer/dist/index.html'));
+    res.sendFile(path.join(__dirname + '/../frontEnd/public/dist/index.html'))
 });
+
 
 app.listen(process.env.PORT || 8080);
