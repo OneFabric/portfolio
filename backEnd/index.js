fadf39bfde765fd184d84ae890b3cb74cbb99942
OneFabric <1neFabric@gmail.com>
2020-06-10 14:21:55 -0700

    Testing without php.

--- backEnd/index.js
+++ backEnd/index.js
@@ -16,7 +16,7 @@ app.get('/', function(req, res) {
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/assets/mail/contact_me.php', function(req, res) {
+app.post('/contactMe', function(req, res) {
     console.log("getting contact_me.php request!")
     console.log(req.body)
     mailer(
