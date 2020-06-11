b06c239967d0de806ffdd782a27c27a3f31897a6
OneFabric <1neFabric@gmail.com>
2020-06-11 13:47:26 -0700

    Added functionality to make the code feed look nice and pretty.

--- backEnd/process/process.js
+++ backEnd/process/process.js
@@ -4,23 +4,54 @@ const fs = require('fs')
 
 var endpoint = API.apiEndpoint
 
-const time = "0 */30 * * * *" // 11 am, every day
+const time = "0 */30 * * * *" // every half hour
 
 function process() {
-    var apiResult = []
+    var apiResult =  []
 
     cron.schedule(
                     time,
                     function() {
                         endPointWrapper() 
-                        fs.writeFile('./frontEnd/public/feed.txt',apiResult, function(err,result) {
-                            if(err) console.log('error!:',err)
+
+                        // remove file if it exists.
+                        fs.unlink('./frontEnd/public/feed.txt', (err) => {
+                            if (err) {
+                              console.error("Problem removing file!: "+err)
+                              return
+                            }
                         })
-                    }
-                 )
+
+                        apiResult.forEach(
+                            element => {
+                                fs.appendFile('./frontEnd/public/feed.txt',
+                                                prettyPrint(element),
+                                                function(err,result) {
+                                                    if(err) { 
+                                                            console.log('error!: Not able to append '+element,err)
+                                                        }
+                                                    })
+                            }
+                        )
+    
+                        }
+                    )
+    
     
     function endPointWrapper() {
-        apiResult = endpoint()
+         apiResult = endpoint()
+    }
+
+
+    function prettyPrint(arrayElement) {
+        var regexExpRepoName = '\/OneFabric\/(.*)\/commits'
+        var regexExpRepoURL = '(.*)->(.*)'
+
+        var repoName = arrayElement.match(regexExpRepoName)[1]
+        var url = "<a href=\""+arrayElement.match(regexExpRepoURL)[1]+"\"\>"+repoName+"</a>"
+        var description = arrayElement.match(regexExpRepoURL)[2]
+
+        return url+" : "+description +"<br>"
     }
 }
 
